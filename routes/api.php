2f2c9b19eb35f986f775e4758f6e05851df4984e
Yousef201x <yousefmohamed2006x@gmail.com>
2024-03-23 01:38:54 +0200

    Routes updated

--- routes/api.php
+++ routes/api.php
@@ -18,15 +18,9 @@ use App\Http\Controllers\Api\AuthController;
 Route::post('register', [AuthController::class, 'register']);
 Route::post('login', [AuthController::class, 'login']);
 
-
-
-
 Route::middleware(['auth:sanctum'])->group(function () {
-    Route::get('test', function () {
-    });
+    Route::post('logout', [AuthController::class, 'logout']);
 });
-
-Route::middleware('auth:sanctum')->post('logout', [AuthController::class, 'logout']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
