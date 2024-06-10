eba1724c3292c5facaffad5ae95a48a2323b7c30
Yousef201x <yousefmohamed2006x@gmail.com>
2024-06-10 14:58:49 +0300

    bug fixed

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -25,7 +25,7 @@ class AuthController extends Controller
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
-            'phone' => $request->phone,
+            // 'phone' => $request->phone,
             'password' => Hash::make($request->password),
         ]);
 
